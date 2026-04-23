2211f432c7d7f8cd3f400d2f814d7669a29078a0
Sarika Priya Das <sarikapriya15@gmail.com>
2026-04-23 18:52:40 +0530

    added logger code

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -5,6 +5,12 @@ namespace azure_app_sarika_vs_1.Pages
 {
     public class PrivacyModel : PageModel
     {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
         public void OnGet()
         {
         }
