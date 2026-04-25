8df00a4be1792ab6f3c84b1c519a02822e4a919b
Sarika Priya Das <sarikapriya15@gmail.com>
2026-04-25 12:32:20 +0530

    Added changes for staging

---
+++ Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_sarika_vs_1.Pages
+{
+    public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
